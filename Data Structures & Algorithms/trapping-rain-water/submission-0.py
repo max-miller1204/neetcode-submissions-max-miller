c33f9c69cdfd98de5704c32a99313227c6b7c54b
Max Miller <maxmiller1204@outlook.com>
2026-06-08 16:43:18 -0400

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        left_max, right_max = height[l], height[r]
+        water = 0
+
+        while l < r:
+            if left_max < right_max:
+                # left side is bottleneck
+                l += 1
+                left_max = max(left_max, height[l])
+                water += left_max - height[l]
+            else:
+                # right side is bottleneck
+                r -= 1
+                right_max = max(right_max, height[r])
+                water += right_max - height[r]
+        return water    
